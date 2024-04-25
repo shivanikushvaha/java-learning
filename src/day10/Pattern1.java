af4d4cd87738c575157a9c0a06e0b8baff89af82
Shivani Kushvaha <shivanikushwaha3172@gmail.com>
2024-04-25 18:51:49 +0530

    day9 commit added

---
+++ src/day10/Pattern1.java
@@ -0,0 +1,16 @@
+package day10;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++)  {
+            for (int j = 4 - i; j > 1; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
