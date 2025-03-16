437fa91cf4b14feba3b7ee2e9ce370d66f50e225
Pushpendra Kushvaha <pushpendra.kushvaha@magnitglobal.com>
2025-03-16 12:29:52 +0530

    observer

---
+++ src/observer/CurrentConditionsDisplay.java
@@ -0,0 +1,14 @@
+package observer;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+
+    @Override
+    public void display() {
+        
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+
+    }
+}
